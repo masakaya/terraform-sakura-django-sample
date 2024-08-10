ae6432babff2859b277a80b2db527a167282fa1b
Masashi Kayahara <masashi.kayahara@arc-connects.com>
2024-08-10 10:49:12 +0900

    さくらのクラウドにてオブジェクトストレージにtfstate管理をしてサーバーを追加する (#4)
    * Update gitignore
    
    * Add Sakura server
    
    * Fix comment out

---
+++ terraform/enviroment/dev/backend.tf
@@ -0,0 +1,16 @@
+terraform {
+  # さくらインターネット用オブジェクトストレージ
+  backend "s3" {
+    bucket = "masakaya-tfstate"
+    key    = "project_name/env/dev/terraform.tfstate"
+    region = "jp-north-1"
+    endpoints = {
+      s3 = "https://s3.isk01.sakurastorage.jp"
+    }
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    skip_region_validation      = true
+    skip_metadata_api_check     = true
+    skip_s3_checksum            = true
+  }
+}
