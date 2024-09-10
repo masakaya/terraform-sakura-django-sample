7d61cb6c13b0f6add5e16e7560362f0058e552cb
Masashi Kayahara <masashi.kayahara@arc-connects.com>
2024-09-10 09:55:13 +0900

    Base Migration

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    id = models.AutoField(primary_key=True)  # オートインクリメントなid
+    name = models.CharField(max_length=100)  # 企業名フィールド
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)  # オートインクリメントなid
+    name = models.CharField(max_length=100)  # nameフィールド
+    password = models.CharField(max_length=100)  # passwordフィールド
+    company_id = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='users')  # company_idを保持
+
+    def __str__(self):
+        return self.name
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,11 +74,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'pgcrypto_test'),
+        'USER': os.environ.get('DB_USER', 'myadmin'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'myadmin_pass'),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '5432'),
     }
 }
 
