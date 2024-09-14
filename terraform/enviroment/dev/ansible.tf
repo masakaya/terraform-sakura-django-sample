afa0508c97f6bb66930606d7a84022ba1e4f3cae
Masashi Kayahara <masashi.kayahara@arc-connects.com>
2024-09-15 08:21:45 +0900

    Add database server (#11)

--- terraform/enviroment/dev/ansible.tf
+++ terraform/enviroment/dev/ansible.tf
@@ -1,28 +1,11 @@
-# Webサーバーのansible_hostを指定
-resource "ansible_host" "web" {
-  depends_on = [module.server["web"]]
-  name       = "web01"
-  groups     = ["web"]
+resource "ansible_host" "servers" {
+  for_each = local.servers
+  name   = each.value.hostname
+  groups = [each.value.suffix]
   variables = {
-    ansible_user                 = "ubuntu"
-    ansible_host                 = module.server["web"].ip_address
-    ansible_ssh_private_key_file = ".ssh/id_rsa_${var.env}"
-    ansible_python_interpreter   = "/usr/bin/python3"
-    ansible_become               = "yes"
-    ansible_become_method        = "sudo"
-    ansible_become_pass          = var.password
-  }
-}
-
-# Webサーバーのansible_hostを指定
-resource "ansible_host" "mng" {
-  depends_on = [module.server["mng"]]
-  name       = "mng01"
-  groups     = ["mng"]
-  variables = {
-    ansible_user                 = "ubuntu"
-    ansible_host                 = module.server["mng"].ip_address
-    ansible_ssh_private_key_file = ".ssh/id_rsa_${var.env}"
+    ansible_user                 = "ubuntu" // 必要に応じてユーザーを指定
+    ansible_host                 = each.value.private_ip
+    ansible_ssh_private_key_file = ".ssh/id_rsa" // 必要に応じてキーを指定
     ansible_python_interpreter   = "/usr/bin/python3"
     ansible_become               = "yes"
     ansible_become_method        = "sudo"
