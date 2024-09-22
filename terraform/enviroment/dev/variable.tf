8786135c4c3939c9d00e601e9661895b870c431e
Masashi Kayahara <masashi.kayahara@arc-connects.com>
2024-09-22 10:32:27 +0900

    Copy certificates file

--- terraform/enviroment/dev/variable.tf
+++ terraform/enviroment/dev/variable.tf
@@ -59,7 +59,12 @@ variable "api_key_id" {
   type        = string
   description = "オートスケール"
   default     = "113601543605" // APIキーの払い出しによって可変なため、tfval化を行うこと
+}
 
+variable "ansible_dir" {
+  type        = string
+  description = "ansible実行パス" 
+  default = "./ansible" 
 }
 
 variable "ansible_dir" {
