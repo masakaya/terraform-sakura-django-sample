504c9015d15d6e4f9258886ef923a3919ec24a87
Masashi Kayahara <masashi.kayahara@arc-connects.com>
2024-11-01 18:37:58 +0900

    Add Batch server. (#28)

--- terraform/enviroment/dev/locals.tf
+++ terraform/enviroment/dev/locals.tf
@@ -13,12 +13,26 @@ locals {
       disk_size        = 20 // 要件に応じて修正
       switch_id        = sakuracloud_internet.router.switch_id
       gateway          = sakuracloud_internet.router.gateway
-      private_ip       = sakuracloud_internet.router.ip_addresses[2]
+      private_ip       = sakuracloud_internet.router.ip_addresses[0]
       filter_id        = sakuracloud_packet_filter.web.id
       core             = 1 // 要件に応じて修正
       memory           = 1 // 要件に応じて修正
       hostname         = "web-01"
       description      = "Web Server"
+      local_private_ip = "192.168.0.2"
+    }
+
+    batch = {
+      suffix           = "batch"
+      disk_size        = 100 // 要件に応じて修正
+      switch_id        = sakuracloud_internet.router.switch_id
+      gateway          = sakuracloud_internet.router.gateway
+      private_ip       = sakuracloud_internet.router.ip_addresses[8]
+      filter_id        = sakuracloud_packet_filter.db.id
+      core             = 1 // 要件に応じて修正
+      memory           = 2 // 要件に応じて修正
+      hostname         = "batch-01"
+      description      = "Batch server"
       local_private_ip = "192.168.0.10"
     }
 
@@ -33,7 +47,7 @@ locals {
       memory           = 2 // 要件に応じて修正
       hostname         = "database-01"
       description      = "Database server"
-      local_private_ip = "192.168.0.20"
+      local_private_ip = "192.168.0.200"
     }
 
     mng = {
@@ -47,7 +61,7 @@ locals {
       memory           = 4 // 要件に応じて修正
       hostname         = "manage-01"
       description      = "Management server"
-      local_private_ip = "192.168.0.30"
+      local_private_ip = "192.168.0.254"
     }
   }
 }
