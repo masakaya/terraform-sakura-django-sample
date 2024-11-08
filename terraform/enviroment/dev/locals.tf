f4cb92c866860254adcff63d99c527485704357b
masakaya <masakaya@users.noreply.github.com>
2024-11-08 01:59:36 +0000

    terraform fmt by Github Actions

--- terraform/enviroment/dev/locals.tf
+++ terraform/enviroment/dev/locals.tf
@@ -8,17 +8,17 @@ locals {
   }
 
   scale_server = {
-    server_count     = var.server_count
-    suffix           = "web"
-    disk_size        = 20 // 要件に応じて修正
-    switch_id        = sakuracloud_internet.router.switch_id
-    gateway          = sakuracloud_internet.router.gateway
-    filter_id        = sakuracloud_packet_filter.web.id
-    core             = 1 // 要件に応じて修正
-    memory           = 1 // 要件に応じて修正
-    disk_plan        = "ssd"
-    hostname         = "web-%s"
-    description      = "Web Server %s"
+    server_count       = var.server_count
+    suffix             = "web"
+    disk_size          = 20 // 要件に応じて修正
+    switch_id          = sakuracloud_internet.router.switch_id
+    gateway            = sakuracloud_internet.router.gateway
+    filter_id          = sakuracloud_packet_filter.web.id
+    core               = 1 // 要件に応じて修正
+    memory             = 1 // 要件に応じて修正
+    disk_plan          = "ssd"
+    hostname           = "web-%s"
+    description        = "Web Server %s"
     local_private_cidr = "192.168.0.0/24"
   }
 
